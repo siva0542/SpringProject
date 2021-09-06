5a0ad0dc02c262fb4b68ac5edcb3e95a0d68078e
siva0542 <sivabhargavakanduru@gmail.com>
2021-09-06 19:47:14 +0530

    Styles added for frontend

--- src/main/java/com/example/makeMyTrip/Travel.java
+++ src/main/java/com/example/makeMyTrip/Travel.java
@@ -3,6 +3,15 @@ package com.example.makeMyTrip;
 public class Travel {
     private String source;
     private String destination;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
     public String getSource() {
         return source;
